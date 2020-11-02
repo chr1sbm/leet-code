ce6f8278c4e2c81a33810e46fe9bdd46f7e1cb98
unknown <cgbejarano@webcreek.com>
2020-11-02 01:07:13 -0500

    Add Minimum Depth Binary Tree solution using BFS

---
+++ MinimumDepthBinaryTree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions
+{
+    public class MinimumDepthBinaryTree
+    {
+        /*  111. Minimum Depth of Binary Tree https://leetcode.com/problems/minimum-depth-of-binary-tree/
+         *  Given a binary tree, find its minimum depth.
+            The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+            NOTE: THIS IS BFS METHOD, IT'S ALSO POSSIBLE USING DFS
+             */
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+            Queue<TreeNode> nodes = new Queue<TreeNode>();
+            int currentLevel = 0;
+            nodes.Enqueue(root);
+
+            while (nodes.Count > 0)
+            {
+                currentLevel++;
+                int qSize = nodes.Count;
+
+                for (var i = 0; i < qSize; i++)
+                {
+                    var temp = nodes.Dequeue();
+
+                    if (temp.left == null && temp.right == null)
+                    {
+                        return currentLevel;
+                    }
+
+                    if (temp.left != null)
+                    {
+                        nodes.Enqueue(temp.left);
+
+                    }
+
+                    if (temp.right != null)
+                    {
+                        nodes.Enqueue(temp.right);
+
+                    }
+
+                }
+
+            }
+
+            return currentLevel;
+        }
+    }
+}
