a1a9f4bcfc5d1952bce5d6fce99fa391d1ddbbf8
chr1sbm <christianb1729@gmail.com>
2020-12-08 00:38:53 -0500

    Add two solutions

---
+++ Arrays/RichestCustomerWealth.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Solutions.Arrays
+{
+    /*   1672. Richest Customer Wealth https://leetcode.com/problems/richest-customer-wealth/
+         You are given an m x n integer grid accounts where accounts[i][j] is 
+         the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. 
+         Return the wealth that the richest customer has.
+         A customer's wealth is the amount of money they have in all their bank accounts.
+         The richest customer is the customer that has the maximum wealth.
+         */
+    public class RichestCustomerWealth
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            int max_value = 0;
+
+            for (var i = 0; i < accounts.Length; i++)
+            {
+                int acum = 0;
+
+                for (var j = 0; j < accounts[i].Length; j++)
+                {
+                    acum += accounts[i][j];
+                }
+
+                if (acum > max_value)
+                {
+                    max_value = acum;
+                }
+
+            }
+
+            return max_value;
+
+        }
+    }
+}
---
+++ Arrays/ShuffleArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions.Arrays
+{
+    /*  1470. Shuffle the Array https://leetcode.com/problems/shuffle-the-array/
+        Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+        Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+         */
+    public class ShuffleArray
+    {
+        public int[] Shuffle(int[] nums, int n)
+        {
+            int[] result = new int[2 * n];
+
+            for (var i = 0; i < n; i++)
+            {
+                result[2 * i] = nums[i];
+                result[2 * i + 1] = nums[n + i];
+            }
+
+            return result;
+
+        }
+    }
+}
