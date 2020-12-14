ffc9353d7e3d9833c0634c2d1a37dfdeb7a4461a
chr1sbm <christianb1729@gmail.com>
2020-12-13 21:37:55 -0500

    Add solution to 1480

---
+++ Arrays/RunningSum1DArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions.Arrays
+{
+    public class RunningSum1DArray
+    { /*1480. Running Sum of 1d Array https://leetcode.com/problems/running-sum-of-1d-array/
+        Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+        Return the running sum of nums.
+         */
+        public int[] RunningSum(int[] nums)
+        {
+            int length = nums.Length;
+            int sum = 0;
+            int index = 0;
+
+            for (var i = 0; i < length; i++)
+            {
+                sum += nums[i];
+                nums[i] = sum;
+                index++;
+            }
+
+            return nums;
+
+        }
+    }
+}
