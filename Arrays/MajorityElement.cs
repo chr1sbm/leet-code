38ed97bf96c09d066741207f700c4d8f99d5edd5
chr1sbm <christianb1729@gmail.com>
2020-12-16 21:42:29 -0500

    Add solution to 169 Majority Element

---
+++ Arrays/MajorityElement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions.Arrays
+{
+    public class MajorityElement
+    {   /*  169. Majority Element https://leetcode.com/problems/majority-element/
+            Given an array of size n, find the majority element.
+            The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+            You may assume that the array is non-empty and the majority element always exist in the array.
+         */
+        public int MajorityElementFunc(int[] nums)
+        {
+            Dictionary<int, int> counters = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (counters.ContainsKey(nums[i]))
+                {
+                    counters[nums[i]]++;
+                }
+
+                else
+                {
+                    counters.Add(nums[i], 1);
+                }
+            }
+
+            foreach (var item in counters)
+            {
+                if (item.Value > nums.Length / 2)
+                {
+                    return item.Key;
+                }
+            }
+
+            return -1;
+
+        }
+    }
+}
