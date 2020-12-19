cbece24e272782f84d6ff30bb9a95957f2a1b182
chr1sbm <christianb1729@gmail.com>
2020-12-18 21:37:40 -0500

    Add solution to 977

---
+++ Two pointers/SquaresSortedArray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions.Two_pointers
+{
+    public class SquaresSortedArray
+    {
+        /*  977. Squares of a Sorted Array https://leetcode.com/problems/squares-of-a-sorted-array/
+         *  Given an integer array nums sorted in non-decreasing order,
+         *  return an array of the squares of each number sorted in non-decreasing order.
+         */
+        public int[] SortedSquares(int[] nums)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            int aux = nums.Length - 1;
+            int[] result = new int[nums.Length];
+
+            while (left <= right)
+            {
+                if (Math.Abs(nums[left]) > Math.Abs(nums[right]))
+                {
+                    result[aux] = nums[left] * nums[left];
+                    left++;
+                }
+
+                else
+                {
+                    result[aux] = nums[right] * nums[right];
+                    right--;
+                }
+
+                aux--;
+
+            }
+
+            return result;
+
+        }
+    }
+}
