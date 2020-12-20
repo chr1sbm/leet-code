90db56a231b766fb6394c431f120dad576f3c8f0
chr1sbm <christianb1729@gmail.com>
2020-12-19 23:48:47 -0500

    Add solution to ReverseString

---
+++ Two pointers/ReverseString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions.Two_pointers
+{
+    public class ReverseString
+    {
+        public void ReverseStringFunc(char[] s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left <= right)
+            {
+                char aux = s[left];
+                s[left] = s[right];
+                s[right] = aux;
+                left++;
+                right--;
+            }
+
+        }
+    }
+}
+
