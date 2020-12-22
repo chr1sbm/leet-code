273a67b66a76897c47fc798e13503e4617106d46
chr1sbm <christianb1729@gmail.com>
2020-12-21 23:26:25 -0500

    Add soltuion to 1512 and 167

---
+++ HashMap/NumberGoodPairs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions.HashMap
+{
+    public class NumberGoodPairs
+    {
+        /*  1512. Number of Good Pairs https://leetcode.com/problems/number-of-good-pairs/
+            Given an array of integers nums.
+            A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+            Return the number of good pairs.
+        */
+        public int NumIdenticalPairs(int[] nums)
+        {
+            // since nums[i] <= 100
+            int[] counters = new int[101];
+            int result = 0;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                counters[nums[i]]++;
+            }
+
+            foreach (int val in counters)
+            {
+                if (val > 0)
+                {
+                    result += val * (val - 1) / 2;
+                }
+
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ Two pointers/TwoSumII.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions.Two_pointers
+{
+    /*  167. Two Sum II - Input array is sorted https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+        Given an array of integers that is already sorted in ascending order,
+        find two numbers such that they add up to a specific target number.
+        The function twoSum should return indices of the two numbers such that they add up to the target,
+        where index1 must be less than index2.
+        Note:
+        Your returned answers (both index1 and index2) are not zero-based.
+        You may assume that each input would have exactly one solution and you may not use the same element twice.
+    */
+    public class TwoSumII
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int left = 0;
+            int right = numbers.Length - 1;
+
+            while (left <= right)
+            {
+                if (numbers[left] + numbers[right] == target)
+                {
+                    return new int[] { left + 1, right + 1 };
+                }
+                else if (numbers[left] + numbers[right] > target)
+                {
+                    right--;
+                }
+
+                else
+                {
+                    left++;
+                }
+            }
+
+            return null;
+        }
+    }
+}
